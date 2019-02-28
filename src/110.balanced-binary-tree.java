3e360f5e09d7ae983ee084dd1a78330755c27b84
Saurav Sharma <Saurav.Sharma@Copart.com>
2019-02-27 18:38:04 -0600

    added some code

---
+++ src/110.balanced-binary-tree.java
@@ -0,0 +1,88 @@
+/*
+ * @lc app=leetcode id=110 lang=java
+ *
+ * [110] Balanced Binary Tree
+ *
+ * https://leetcode.com/problems/balanced-binary-tree/description/
+ *
+ * algorithms
+ * Easy (40.33%)
+ * Total Accepted:    294K
+ * Total Submissions: 729K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * Given a binary tree, determine if it is height-balanced.
+ * 
+ * For this problem, a height-balanced binary tree is defined as:
+ * 
+ * 
+ * a binary tree in which the depth of the two subtrees of every node never
+ * differ by more than 1.
+ * 
+ * 
+ * Example 1:
+ * 
+ * Given the following tree [3,9,20,null,null,15,7]:
+ * 
+ * 
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ * 
+ * Return true.
+ * 
+ * Example 2:
+ * 
+ * Given the following tree [1,2,2,3,3,null,null,4,4]:
+ * 
+ * 
+ * ⁠      1
+ * ⁠     / \
+ * ⁠    2   2
+ * ⁠   / \
+ * ⁠  3   3
+ * ⁠ / \
+ * ⁠4   4
+ * 
+ * 
+ * Return false.
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+
+    boolean isBalanced = true;
+    public boolean isBalanced(TreeNode root) {
+        
+        if(root == null) return true;
+        if(root.left == null && root.right == null)  return true;
+
+        validateLength(root);
+        return isBalanced;
+    }
+
+    public int validateLength(TreeNode root) {
+
+        if(!isBalanced) return 0;
+        if(root == null) return 0;
+
+        int leftChild = validateLength(root.left);
+        int rightChild = validateLength(root.right);
+
+        if(Math.abs(leftChild - rightChild) > 1) {
+            isBalanced = false;
+        }
+        return 1 + Math.max(leftChild, rightChild);
+    }
+}
+
--- src/141.linked-list-cycle.java
+++ src/141.linked-list-cycle.java
@@ -78,5 +78,15 @@
 public class Solution {
     public boolean hasCycle(ListNode head) {
         
+        if(head == null || head.next == null) return false;
+
+        ListNode slow = head, fast = head;
+
+        while(fast.next.next != null) {
+            if(fast == slow) return true;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return false;
     }
 }
